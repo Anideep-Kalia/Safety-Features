e61cbc5489f2c3acf1126d49e12d8a1d4e412463
soitcoder <rahsut413@gmail.com>
2024-03-28 14:57:45 +0530

    admin-folder-structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage"
-
+import Admin from "./pages/admin/AdminPage"
+import "./App.css"
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Homepage />} />
+        <Route path='/admin' element={<Admin/>}/>
       </Routes>
     </>
   );
---
+++ frontend/src/pages/admin/AdminNavbar.jsx
@@ -0,0 +1,10 @@
+const Navbar=()=>{
+    return <>
+    
+<h1 className="font-bold text-xl underline color text-blue-600">
+      Hello world!
+    </h1>
+    </>
+}
+
+export default Navbar
