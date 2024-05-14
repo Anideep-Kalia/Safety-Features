c497c7a6bb47dce9d362c42cd5817435b55cbf0c
Yash Raj Gupta <yashraj.guptadev@gmail.com>
2024-05-14 13:44:29 +0530

    basic ui changes

--- frontend/src/components/admin/AdminSidebar.jsx
+++ frontend/src/components/admin/AdminSidebar.jsx
@@ -8,7 +8,7 @@ import { MdOutlineDashboard } from "react-icons/md";
 // import { MdDeleteOutline } from "react-icons/md";
 // import { MdOutlineAddLocationAlt } from "react-icons/md";
 
-import React from "react";
+import React, { useState } from "react";
 import { FiShoppingBag } from "react-icons/fi";
 import { GrWorkshop } from "react-icons/gr";
 import { RxDashboard } from "react-icons/rx";
@@ -17,28 +17,64 @@ import { Link } from "react-router-dom";
 import { HiOutlineUserGroup, HiOutlineViewGridAdd } from "react-icons/hi";
 import { BsHandbag } from "react-icons/bs";
 import { MdOutlineLocalOffer } from "react-icons/md";
-import { AiOutlineSetting, AiOutlineUserAdd, AiOutlineEdit } from "react-icons/ai";
+import { AiOutlineSetting, AiOutlineUserAdd, AiOutlineEdit, AiFillEdit  } from "react-icons/ai";
 import { MdDashboard } from "react-icons/md";
-
+import { MdOutlineLocationOn, MdLocationOn } from "react-icons/md";
+import { IoArchiveOutline,  IoArchive } from "react-icons/io5";
+import { AiOutlineDelete } from "react-icons/ai";
+import { MdMovieEdit } from "react-icons/md";
+import AddLocationPopup from "./AddLocationPopup";
 
 const AdminSideBar = ({ active }) => {
+
+  const [open, setOpen] = useState(false);
+
+  const toggleOpen  = () => {
+    setOpen(!open);
+  }
+
   return (
-    <div className="w-full h-[85vh] bg-white shadow-sm sticky top-0 left-0 z-5">
-      {/* single item */}
+    <div className="w-full h-[85vh] bg-white  sticky top-0 left-0 p-2">
+      <div className="px-6 py-2 mb-2">
+        <div className="flex flex-col  justify-center items-center mb-5">
+          <img src="/pic.png" 
+          alt="imge"
+          />
+          <h1 className="text-center text-[14px] font-semibold">Shri Bhajan Lal Sharma 
+Hon’ble Chief Minister of Rajasthan</h1>
+        </div>
+        <div className="  rounded-[50px] text-white bg-[#4E7690] py-3 items-center ">
+          <button className="flex justify-center items-center  w-full gap-2" onClick={toggleOpen}>
+            <img src="/Vector3.svg"
+            alt="im"
+
+            />
+
+            <p className="text-[14px]">Add New Location</p>
+          </button>
+          <div className=" ">
+
+          <AddLocationPopup open={open}/>
+          </div>
+          
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <h1 className="text-[14px] text-[#4E7690] font-medium py-2 px-6">Menu</h1>
       <div className="w-full flex items-center p-4">
-        <Link to="/admin-dashboard" className="w-full flex items-center">
+        <Link to="/admin-dashboard" className="w-full  px-2 flex items-center">
           < MdDashboard
-            size={30}
-            color={`${active === 1 ? "crimson" : "#555"}`}
+            size={20}
+            color={`#4E7690`}
             className={`${active === 1 ? 'visible': 'hidden' }`}
           />
           < MdOutlineDashboard
-            size={30}
+            size={20}
             color={`#71829B`}
-        
+            className={`${active === 1 ? 'hidden' : 'visible' }`}
           />
           <h5
-            className={`hidden md:block pl-2 text-[18px]  text-[#71829B] ${active === 1 ? "font-bold" : "font-normal"
+            className={`hidden md:block pl-2 text-[16px]   ${active === 1 ? "font-semibold text-[#4E7690]" : " font-medium text-[#71829B]"
               }`}
             
           >
@@ -48,14 +84,20 @@ const AdminSideBar = ({ active }) => {
       </div>
 
       <div className="w-full flex items-center p-4">
-        <Link to="/admin-create-quiz" className="w-full flex items-center">
-          <HiOutlineViewGridAdd
-            size={30}
-            color={`${active === 2 ? "crimson" : "#555"}`}
+        <Link to="/admin-location" className="w-full   px-2 flex items-center">
+        <MdLocationOn
+            size={20}
+            color={`#4E7690`}
+            className={`${active === 2 ? 'visible': 'hidden' }`}
+          />
+          <MdOutlineLocationOn
+            size={20}
+            color={`#71829B`}
+            className={`${active !== 2 ? 'visible': 'hidden' }`}
           />
           <h5
-            className={`hidden md:block pl-2 text-[18px] font-[400] ${active === 2 ? "text-[crimson]" : "text-[#555]"
-              }`}
+           className={`hidden md:block pl-2 text-[16px]   ${active === 2 ? "font-semibold text-[#4E7690]" : " font-medium text-[#71829B]"
+          }`}
           >
             Location
           </h5>
@@ -63,14 +105,22 @@ const AdminSideBar = ({ active }) => {
       </div>
 
       <div className="w-full flex items-center p-4">
-        <Link to="/admin-quiz" className="w-full flex items-center">
-          <FaLayerGroup
-            size={30}
-            color={`${active === 3 ? "crimson" : "#555"}`}
+        <Link to="/admin-access" className="w-full   px-2 flex items-center">
+          <img
+          src = "/Vector.svg"
+          alt="access"
+            size={20}
+            className={`${active !== 3 ? 'visible': 'hidden' }`}
+          />
+            <img
+          src = "/Vector2.svg"
+          alt="access"
+            size={20}
+            className={`${active === 3 ? 'visible': 'hidden' }`}
           />
           <h5
-            className={`hidden md:block pl-2 text-[18px] font-[400] ${active === 3 ? "text-[crimson]" : "text-[#555]"
-              }`}
+      className={`hidden md:block pl-2 text-[16px]   ${active === 3 ? "font-semibold text-[#4E7690]" : " font-medium text-[#71829B]"
+    }`}
           >
             Access
           </h5>
@@ -78,14 +128,20 @@ const AdminSideBar = ({ active }) => {
       </div>
 
       <div className="w-full flex items-center p-4">
-        <Link to="/admin-user" className="w-full flex items-center">
-          <HiOutlineUserGroup
-            size={30}
-            color={`${active === 4 ? "crimson" : "#555"}`}
+        <Link to="/admin-edit-history" className="w-full  flex items-center  px-2">
+          <AiOutlineEdit
+            size={20}
+            color={`#71829B`}
+            className={`${active !== 4 ? 'visible': 'hidden' }`}
+          />
+          <AiFillEdit
+            size={20}
+            color={`#4E7690`}
+            className={`${active === 4 ? 'visible': 'hidden' }`}
           />
           <h5
-            className={`hidden md:block pl-2 text-[18px] font-[400] ${active === 4 ? "text-[crimson]" : "text-[#555]"
-              }`}
+        className={`hidden md:block pl-2 text-[16px]   ${active === 4 ? "font-semibold text-[#4E7690]" : " font-medium text-[#71829B]"
+      }`}
           >
             Edit History
           </h5>
@@ -94,39 +150,48 @@ const AdminSideBar = ({ active }) => {
 
       <div className="w-full flex items-center p-4">
         <Link
-          to="/admin-update-password"
-          className="w-full flex items-center"
+          to="/admin-archive"
+          className="w-full flex items-center   px-2"
         >
-          <AiOutlineEdit
-            size={30}
-            color={`${active === 5 ? "crimson" : "#555"}`}
+          <IoArchive
+            size={20}
+            color={`#4E7690`}
+            className={`${active === 5 ? 'visible': 'hidden' }`}
+          />
+           <IoArchiveOutline
+            size={20}
+            color={`#71829B`}
+            className={`${active !== 5 ? 'visible': 'hidden' }`}
           />
           <h5
-            className={`hidden md:block pl-2 text-[18px] font-[400] ${active === 5 ? "text-[crimson]" : "text-[#555]"
-              }`}
+       className={`hidden md:block pl-2 text-[16px]   ${active === 5 ? "font-semibold text-[#4E7690]" : " font-medium text-[#71829B]"
+      }`}
           >
             Archive
           </h5>
         </Link>
       </div>
 
-      <div className="w-full flex items-center p-4">
+      <div className="w-full flex items-center px-4  ">
         <Link
-          to="/profile"
-          className="w-full flex items-center"
+          to="/admin-delete-location"
+          className={`w-fit py-3 px-2 rounded-xl flex items-center ${active === 6 ? "bg-[#F44336]": "bg-[#FDEBEA] "} `} 
         >
-          <AiOutlineSetting
-            size={30}
-            color={`${active === 8 ? "crimson" : "#555"}`}
+          <AiOutlineDelete
+            size={20}
+            color={`${active === 6 ? "#FDD9D7" : "#F44336"}`}
           />
           <h5
-            className={`hidden md:block pl-2 text-[18px] font-[400] ${active === 8 ? "text-[crimson]" : "text-[#555]"
-              }`}
+         className={`hidden md:block pl-2 text-[16px]   ${active === 6 ? "font-semibold text-[#FDD9D7]" : "font-medium text-[#F44336]"
+        }`}
           >
-            Delete Location
+            Deleted Locations
           </h5>
         </Link>
       </div>
+      </div>
+      {/* single item */}
+      
 
     </div>
   );
--- frontend/src/pages/admin/AdminLocationPage.jsx
+++ frontend/src/pages/admin/AdminLocationPage.jsx
@@ -1,12 +1,10 @@
 
+import AdminLocation from "../../components/admin/AdminLocation";
 import AdminNavbar from "../../components/admin/AdminNavbar"
 import AdminSidebar from "../../components/admin/AdminSidebar"
-import AddNewLocationBar from "../../components/admin/AddNewLocationBar";
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import AdminDashboard from '../../components/admin/AdminDashboard';
-import AddLocationPopup from "../../components/admin/AddLocationPopup";
-const Admin=()=>{
+const AdminLocationPage=()=>{
  const navigate = useNavigate();
 
     useEffect(  ()=>{
@@ -23,12 +21,11 @@ const Admin=()=>{
         <div className="w-full flex">
         <div className="flex items-start justify-between w-full mt-[4.5rem]">
           <div className="w-[80px] md:w-[300px] sticky top-[5rem]">
-            <AdminSidebar active={1} />
+            <AdminSidebar active={2} />
           </div>
           <div className="flex flex-col justify-center relative w-full p-4  mt-2">
             <div>
-            <AdminDashboard />
-            {/* <AddLocationPopup/> */}
+            <AdminLocation/>
             </div>
            
           </div>
@@ -40,4 +37,4 @@ const Admin=()=>{
     )
 }
 
-export default Admin
+export default AdminLocationPage
